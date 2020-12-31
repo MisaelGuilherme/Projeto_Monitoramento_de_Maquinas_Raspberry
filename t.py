92a9c9d36a94bc54dbe49d2bd77721162858f626
Misael Guilherme <63935572+MisaelGuilherme@users.noreply.github.com>
2020-12-31 11:38:03 -0300

    Criando Pastas Para as Fontes e Banco de Dados

---
+++ t.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+jane = Tk()
+# Resolução do Sistema
+largura_screen = jane.winfo_screenwidth()
+altura_screen = jane.winfo_screenheight()
+
+print(f'Lagura da tela: {largura_screen}')
+print(f'Altura da tela: {altura_screen}')
+
+jane.mainloop()
