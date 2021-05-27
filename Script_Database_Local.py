7c5196f3b8f31ad927e68fe0bbeb7eae2a98b2e5
Misael Guilherme <63935572+MisaelGuilherme@users.noreply.github.com>
2021-05-27 19:23:32 -0300

    Igualitando Database Local com Database Servidor

--- Script_Database_Local.py
+++ Script_Database_Local.py
@@ -15,21 +15,21 @@ def create_tabels():
                                 (ID varchar(2) NOT NULL DEFAULT 'ID',
                                 Operador varchar(30) NOT NULL DEFAULT '',
                                 CPF varchar(11) NOT NULL DEFAULT '00000000000',
-                                HoraLogin varchar(8) NOT NULL DEFAULT '00:00:00',
-                                HoraInicial varchar(8) NOT NULL DEFAULT '00:00:00',
+                                HoraLogin time NOT NULL DEFAULT '00:00:00',
+                                HoraInicial time NOT NULL DEFAULT '00:00:00',
                                 DataInicial date NOT NULL DEFAULT '0000-00-00',
-                                HoraFinal varchar(8) NOT NULL DEFAULT '00:00:00',
+                                HoraFinal time NOT NULL DEFAULT '00:00:00',
                                 DataFinal date NOT NULL DEFAULT '0000-00-00',
-                                TempGasto varchar(8) NOT NULL DEFAULT '00:00:00',
-                                TempProgramado varchar(8) NOT NULL DEFAULT '00:00:00',
-                                OS varchar(8) NOT NULL DEFAULT '',
-                                CodigoPeca varchar(8) NOT NULL DEFAULT '',
+                                TempGasto time NOT NULL DEFAULT '00:00:00',
+                                TempProgramado time NOT NULL DEFAULT '00:00:00',
+                                OS varchar(13) NOT NULL DEFAULT '',
+                                CodigoPeca varchar(13) NOT NULL DEFAULT '',
                                 CodigoOperacao varchar(3) NOT NULL DEFAULT '',
-                                TempGastoExt varchar(8) NOT NULL DEFAULT '00:00:00',
-                                VezTempExt varchar(8) NOT NULL DEFAULT '0',
-                                TempOperando varchar(8) NOT NULL DEFAULT '00:00:00',
+                                TempGastoExt time NOT NULL DEFAULT '00:00:00',
+                                VezTempExt tinyint(4) NOT NULL DEFAULT '0',
+                                TempOperando time NOT NULL DEFAULT '00:00:00',
                                 Tipo varchar(15) NOT NULL DEFAULT '',
-                                Quant int(4) NOT NULL DEFAULT 1,
+                                Quant mediumint(9) NOT NULL DEFAULT '1',
                                 Maquina varchar(10) NOT NULL DEFAULT 'Máquina 00')
                                 
                             ''')
@@ -44,31 +44,32 @@ def create_tabels():
                                 CodigoOperacao varchar(3) NOT NULL DEFAULT '',
                                 OS varchar(8) NOT NULL DEFAULT '',
                                 MotivoPause varchar(20) NOT NULL DEFAULT '',
-                                HoraPause varchar(8) NOT NULL DEFAULT '',
+                                HoraPause time NOT NULL DEFAULT '00:00:00',
                                 DataPause date NOT NULL DEFAULT '0000-00-00',
-                                HoraRetomada varchar(8) NOT NULL DEFAULT '',
+                                HoraRetomada time NOT NULL DEFAULT '00:00:00',
                                 DataRetomada date NOT NULL DEFAULT '0000-00-00',
                                 TempMarcadoCron varchar(8) NOT NULL DEFAULT '',
-                                TempGastoProg varchar(8) NOT NULL DEFAULT '00:00:00',
-                                TempGastoExt varchar(8) NOT NULL DEFAULT '00:00:00',
-                                VezTempExt varchar(2) NOT NULL DEFAULT '0',
+                                TempGastoProg time NOT NULL DEFAULT '00:00:00',
+                                TempGastoExt time NOT NULL DEFAULT '00:00:00',
+                                VezTempExt tinyint(4) NOT NULL DEFAULT '0',
                                 UltimTempAdd varchar(8) NOT NULL DEFAULT '00:00:00',
-                                TempProg varchar(8) NOT NULL DEFAULT '',
+                                TempProg time NOT NULL DEFAULT '00:00:00',
                                 CorTela varchar(8) NOT NULL DEFAULT '',
-                                Hora_Login varchar(8) NOT NULL DEFAULT '00:00:00',
-                                Hora_Inicial varchar(8) NOT NULL DEFAULT '00:00:00',
+                                Hora_Login time NOT NULL DEFAULT '00:00:00',
+                                Hora_Inicial time NOT NULL DEFAULT '00:00:00',
                                 Data_Inicial date NOT NULL DEFAULT '0000-00-00',
                                 TempOperando varchar(8) NOT NULL DEFAULT '00:00:00',
                                 Tipo varchar(14) NOT NULL DEFAULT '',
                                 Quant int(4) NOT NULL DEFAULT 1,
-                                Maquina varchar(10) NOT NULL DEFAULT 'Máquina 00')
+                                Maquina varchar(10) NOT NULL DEFAULT 'Máquina 00',
+                                repairTemp varchar(8) NOT NULL DEFAULT '00:00:00')
                             
                             ''')
         
         print('BANCO DE DADOS LOCAL CRIADO')
         
-    except:
-        print('Erro ao criar Banco de Dados Local')
+    except Exception as erro:
+        print('Erro ao criar Banco de Dados Local', erro)
 
 #Invocando função
 create_tabels()
